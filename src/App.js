bb86a530719ef7fceab1d3c5c81a6e8bb9472b89
Kalpesh <kalpeshchoudhary902@gmail.com>
2020-12-08 19:29:55 +0530

    dark mode

--- src/App.js
+++ src/App.js
@@ -48,11 +48,11 @@ function App() {
   }
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full">
           <div>
             <img className="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/v1/workflow-mark-on-white.svg" alt="Workflow"/>
-            <h2 className="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
+            <h2 className="mt-6 text-center text-3xl leading-9 font-extrabold text-white">
               URL Snip
             </h2>
           </div>
